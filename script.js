3d5593b875f37dc6cf6b6a35196fa02ee657191a
Simms-creator <adjeibisimms76@gmail.com>
2026-03-05 12:34:12 +0000

    deploy

---
+++ script.js
@@ -0,0 +1,13 @@
+// Welcome message
+function welcomeMessage() {
+  alert("Welcome to Appstine Tech! We make technology easier for you.");
+}
+
+// Show message when the page loads
+window.onload = welcomeMessage;
+
+
+// Button action example
+function contactMessage() {
+  alert("Thank you for contacting Appstine Tech. We will get back to you soon.");
+}
